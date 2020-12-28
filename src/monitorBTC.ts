a0f8eb17e5bf1a6957809acacb13e4992589e9df
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-12-28 10:48:36 -0300

    Alert Top and Dow Variation

--- src/monitorBTC.ts
+++ src/monitorBTC.ts
@@ -5,10 +5,18 @@ import sendMail from './sendMail';
 
 const intervalToCheck = parseInt(`${process.env.INTERVAL_TO_CHECK}`);
 
-const getVariacaoInf = (last: number, high: number) => {
-  return (1 - last / high) * -1;
+const limiteVarDow = Number(`${process.env.BTC_LIMIT_VAR_DOWN}`) / 100;
+const limiteVarTop = Number(`${process.env.BTC_LIMIT_VAR_TOP}`) / 100;
+const BTCBase = Number(`${process.env.BTC_BASE_IN_BRL}`);
+
+const calculateVariation = (last: number) => {
+  const variation = (last / BTCBase);
+  let varTopDow = variation >= 1
+    ? (variation - 1) * 100
+    : (1 - variation) * -100;
+
+  return varTopDow;
 }
-const limiteInf = parseInt(`${process.env.BTC_ALERT_DOWN}`) / 100;
 
 interface IOriginalTicker {
   high: string;
@@ -21,7 +29,7 @@ interface IOriginalTicker {
   date: number;
 }
 
-const formatter = new Intl.NumberFormat('pt-BR', {
+export const formatter = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
   minimumFractionDigits: 2
@@ -34,23 +42,47 @@ const htmlMessage = (parms: IOriginalTicker) => {
   const ultima = formatter.format(Number(last));
   const tickerDate = new Date(date * 1000).toUTCString();
 
-  const variacaoInf = getVariacaoInf(Number(last), Number(high));
+  const variacao = calculateVariation(Number(last));
 
   let html = `${tickerDate} <h3> Bitcoin (BTC):</h3>`
   html += `<ul><li><strong>Ultima cotação ${ultima}</strong></li>`;
-  html += `<li>Menor cotação ${menor}</li>`;
-  html += `<li>Maior cotação em ${maior}</li>`;
-  html += `<li>Variação ${(variacaoInf * 100).toFixed(2)}% (ultima/maior)</li></ul>`;
+  html += `<li>Menor valor ${menor}</li>`;
+  html += `<li>Maior valor ${maior}</li>`;
+  html += `<li>Variação ${(variacao).toFixed(2)}% (ultima/Base)</li></ul>`;
 
-  html += `<br>Limite alerta configurado: ${(limiteInf * 100).toFixed(2)}%.`;
+  html += `<br>Base BTC: ${formatter.format(Number(BTCBase))}.`;
+  html += `<br>Limite alerta Top: ${(limiteVarTop * 100).toFixed(2)}%.`;
+  html += `<br>Limite alerta Dow: ${(limiteVarDow * 100).toFixed(2)}%.`;
   html += `<br><h4>Ticker</h4><pre>${JSON.stringify(parms, null, 2)}</pre>`
-
 
   return html;
 }
 
+export const checkAndAlert = async (originalTicker: IOriginalTicker) => {
+
+  const variacao = calculateVariation(Number(originalTicker.last));
+  console.log(`   Base: ${formatter.format(Number(BTCBase))} Variação ${(variacao).toFixed(2)}%`)
+  console.log(`   Limite Top: ${(limiteVarTop * 100).toFixed(2)}%.`);
+  console.log(`   Limite Dow: ${(limiteVarDow * 100).toFixed(2)}%.`);
+
+  const alertTop = ((variacao >= 0) && (variacao >= limiteVarTop * 100));
+  const alertDow = ((variacao < 0) && (variacao <= limiteVarDow * -100));
+
+  console.log(variacao, limiteVarTop, limiteVarDow, (variacao >= limiteVarTop), (variacao <= limiteVarDow * -1))
+  const subject = alertTop
+    ? 'BTC-Bot Alert (TOP)'
+    : alertDow
+      ? 'BTC-Bot Alert (DOW)'
+      : 'BTC-Bot Alert';
+
+  if (alertTop || alertDow) {
+    const html = htmlMessage(originalTicker);
+    await sendMail({ subject, html })
+  }
+}
+
 const monitorBTC = () => {
-  console.log(`Every ${intervalToCheck} seconds check BTC value`);
+  console.log(`Every ${intervalToCheck} seconds check BTC value.`);
   let isRunning = false;
   try {
     const job = new CronJob(
@@ -64,15 +96,13 @@ const monitorBTC = () => {
               .get('https://www.mercadobitcoin.net/api/BTC/ticker/');
             const originalTicker = data.ticker as IOriginalTicker;
             const tickerDate = new Date(originalTicker.date * 1000).toUTCString();
-            console.log('originalTicker', originalTicker, tickerDate);
+            console.log('\nCheck in ', tickerDate, 'LastValue: ', formatter.format(Number(originalTicker.last)));
 
-            const variacaoInf = getVariacaoInf(Number(originalTicker.last), Number(originalTicker.high));
-            console.log(`Variação ${(variacaoInf * 100).toFixed(2)}% | Limite ${(limiteInf * 100).toFixed(2)}%.`)
-
-            if (variacaoInf * -1 >= limiteInf) { // identificar queda maior que X%
+            await checkAndAlert(originalTicker);
+            /*if (variacaoInf * -1 >= limiteInf) { // identificar queda maior que X%
               const html = htmlMessage(originalTicker);
               await sendMail({ subject: 'BTC-Bot Alert', html })
-            }
+            }*/
             isRunning = false;
           }, 1000);
         }
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { formatter, checkAndAlert } from './monitorBTC'
+
 const router = Router();
 
 router.get('/btc-bot/health', (req, res, next) => {
@@ -7,7 +9,10 @@ router.get('/btc-bot/health', (req, res, next) => {
     message: `${process.env.MS_NAME} is up and running!`,
     checkInterval: `Every ${process.env.INTERVAL_TO_CHECK} seconds`,
     mailRecipient: `${(process.env.MAIL_TO)?.substring(0, 4)}***`,
-    downLimiteAlert: `${process.env.BTC_ALERT_DOWN}%`,
+    downLimiteAlert: `${process.env.BTC_LIMIT_VAR_DOWN}%`,
+    topLimiteAlert: `${process.env.BTC_LIMIT_VAR_TOP}%`,
+    BTCBase: `${formatter.format(Number(process.env.BTC_BASE_IN_BRL))}`,
+    function: `${checkAndAlert.toString()}`
   })
 });
 
